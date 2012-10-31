af99b632847e66f27c2cf31759c1ad2e3b5e05d2
Bradlee Speice <bspeice@uncc.edu>
2012-10-30 20:26:34 -0400

    Update the baseAlgorithm class

--- baseAlgorithm.java
+++ baseAlgorithm.java
@@ -1,18 +1,16 @@
 public class baseAlgorithm{
 	
-	int memoryBlock[];
+	int[] memoryBlock;
 	
-	void baseAlgorithm(){
-		
+	void baseAlgorithm(int memorySize){
+		/* Constructor needed for each algorithm */
+		memoryBlock = new int[memorySize];
 	}
 	
 	void allocate(){
-		
+		/* This method to be overloaded by each algorithm */
 	}
 	void deallocate(){
-		
-	}
-	void createThread(){
 		
 	}
 }
